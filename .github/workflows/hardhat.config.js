9a7dfc5e875418a7de779ff3f39e5545baab90ed
Boomchain Labs <202796817+BoomchainLabs@users.noreply.github.com>
2025-05-25 15:41:07 +0100

    Update hardhat.config.js
    Signed-off-by: Boomchain Labs  <202796817+BoomchainLabs@users.noreply.github.com>

--- .github/workflows/hardhat.config.js
+++ .github/workflows/hardhat.config.js
@@ -4,16 +4,10 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.17",
   networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545",
+    tenderlyMainnet: {
+      url: process.env.TENDERLY_MAINNET_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY].filter(Boolean),
     },
-    goerli: {
-      url: process.env.GOERLI_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY],
-    },
-    mainnet: {
-      url: process.env.MAINNET_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY],
-    },
+    // Other network configs as needed
   },
 };
